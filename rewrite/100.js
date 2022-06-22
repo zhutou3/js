f6b5319107b8db882cc872c63a605a59d33edde6
zhutou3 <85608669+zhutou3@users.noreply.github.com>
2022-06-23 01:11:58 +0800

    Create 100.js

---
+++ rewrite/100.js
@@ -0,0 +1,4 @@
+# > 拦截100解锁永久超级会员
+^https?:\/\/tagit\.hyhuo\.com\/cypt\/block100\/get_vip_info$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/Block100FVIPCrack.js
+
+hostname = tagit.hyhuo.com
